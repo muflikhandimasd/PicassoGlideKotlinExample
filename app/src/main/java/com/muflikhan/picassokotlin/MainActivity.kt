7b98ef03a21687a7cff7ea18918f7492cfcedbb0
Muflikhan Dimas Dwiprayogi <muflikhan.dimas@gmail.com>
2022-03-18 08:41:52 +0700

    Initial commit

---
+++ app/src/main/java/com/muflikhan/picassokotlin/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.muflikhan.picassokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.squareup.picasso.Picasso
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private lateinit var imageView2: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.imageView)
+        imageView2 = findViewById(R.id.imageView2)
+
+        Picasso.get().load("https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1420&q=80")
+            .placeholder(R.drawable.placeholder)
+            .error(R.drawable.placeholder)
+            .into(imageView)
+
+        Glide.with(this).load("https://images.unsplash.com/photo-1526666923127-b2970f64b422?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80")
+            .placeholder(R.drawable.placeholder)
+            .error(R.drawable.placeholder)
+            .into(imageView2)
+    }
+}
